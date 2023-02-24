c92d7eb46a235ea624fb273b9957dec3d6ed2b3b
Jalwa .U.K <henna.jalwa-u-k@capgemini.com>
2023-02-24 11:11:38 +0530

    added json

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -7,7 +7,7 @@ import org.testng.annotations.DataProvider;
 
 
 @CucumberOptions(features ="src/test/resources/features",
-        glue = "steps",publish = true)
+        glue = "steps",publish = true, plugin = {"pretty","html:target/cucumber_ny-reports.html","json:target/cucumber_ny-reports.json"},monochrome =true)
 public class TestRunner extends AbstractTestNGCucumberTests {
     @DataProvider(parallel = true)
     public Object[][] scenarios(){
